7b0dd2dd7949400e80949a1b4ebebcc61d6f6822
rafael <raafael.mooraes@gmail.com>
2020-02-01 11:15:15 -0300

    Check if is dead system created.

---
+++ UnityProject/Assets/Scripts/Game/System/S_CheckIfIsDead.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+public class S_CheckIfIsDead : JobComponentSystem
+{
+
+    private EndSimulationEntityCommandBufferSystem m_EndSimulationEntityCommandBufferSystem;
+    private EntityQuery m_GetAliveEntitiesNotDeadQuery;
+
+    protected override void OnCreate()
+    {
+        m_EndSimulationEntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+        m_GetAliveEntitiesNotDeadQuery = GetEntityQuery(ComponentType.ReadOnly<C_Life>(), ComponentType.Exclude<T_IsDead>());
+    }
+
+    public struct CheckLifeJob : IJobForEachWithEntity<C_Life>
+    {
+        public EntityCommandBuffer EntityCommandBuffer;
+        public void Execute(Entity entity, int index, [ReadOnly]ref C_Life c_Life)
+        {
+            if (c_Life.actualLife <= 0)
+            {
+                EntityCommandBuffer.AddComponent<T_IsDead>(entity, new T_IsDead());
+            }
+        }
+    }
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        
+        JobHandle jobHandle = new CheckLifeJob
+        {
+            EntityCommandBuffer = m_EndSimulationEntityCommandBufferSystem.CreateCommandBuffer()
+        }.Schedule(m_GetAliveEntitiesNotDeadQuery, inputDeps);
+        return jobHandle;
+    }
+}
