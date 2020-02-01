3fe9e0bbf6475563b39326a57427629ed8690135
Felipexox <felipeaa100@gmail.com>
2020-02-01 10:30:26 -0300

    Removed Asmdf

--- UnityProject/Assets/Scripts/Game/Component/C_SpawnRequest.cs
+++ UnityProject/Assets/Scripts/Game/Component/C_SpawnRequest.cs
@@ -1,18 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Entities;
 using UnityEngine;
 
-public class C_SpawnRequest : MonoBehaviour
+public class C_SpawnRequest : IComponentData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
