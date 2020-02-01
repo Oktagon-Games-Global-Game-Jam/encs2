739af4ce9653178018ac00d40f07fd3c12c80c18
Felipexox <felipeaa100@gmail.com>
2020-02-01 10:56:09 -0300

    Spawn System Base   -

--- UnityProject/Assets/Scripts/Game/Component/C_SpawnRequest.cs
+++ UnityProject/Assets/Scripts/Game/Component/C_SpawnRequest.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
-public class C_SpawnRequest : IComponentData
+public struct C_SpawnRequest : IComponentData
 {
-
+    public Entity Reference;
+    public float3 Position; // Start Position At World
+    public byte Direction; // -1 left / 1 right
 }
--- UnityProject/Assets/Scripts/Game/System/S_Spawn.cs
+++ UnityProject/Assets/Scripts/Game/System/S_Spawn.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
-public class S_Spawn : MonoBehaviour
+public class S_Spawn : JobComponentSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    private EndSimulationEntityCommandBufferSystem m_EndSimulationEntityCommandBufferSystem;
+    private EntityQuery m_Query;
+    protected override void OnCreate()
     {
-        
+        base.OnCreate();
+        m_EndSimulationEntityCommandBufferSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+        m_Query = GetEntityQuery(new ComponentType[]
+        {
+            ComponentType.ReadWrite<C_SpawnRequest>(), 
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
         
+        J_SpawnJob tSpawnJob = new J_SpawnJob
+        {
+            CommandBuffer = m_EndSimulationEntityCommandBufferSystem.CreateCommandBuffer(),
+            SpawnRequestChunk = GetArchetypeChunkComponentType<C_SpawnRequest>()
+        };
+
+        return tSpawnJob.Schedule(m_Query, inputDeps);
+    }
+
+    public struct J_SpawnJob : IJobChunk
+    {
+        public ArchetypeChunkComponentType<C_SpawnRequest> SpawnRequestChunk;
+        public EntityCommandBuffer CommandBuffer;
+        public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+        {
+            NativeArray<C_SpawnRequest> tSpawnRequestArray = chunk.GetNativeArray(SpawnRequestChunk);
+            for (int i = 0; i < chunk.Count; i++)
+            {
+                Entity tSpawnedEntity = CommandBuffer.Instantiate(tSpawnRequestArray[i].Reference);
+                
+                CommandBuffer.SetComponent(tSpawnedEntity, new Translation
+                {
+                    Value = tSpawnRequestArray[i].Position
+                });
+                CommandBuffer.SetComponent(tSpawnedEntity, new RotationEulerXYZ
+                {
+                    Value = new float3(0, 90 * tSpawnRequestArray[i].Direction, 0)
+                });
+            }
+        }
     }
 }
