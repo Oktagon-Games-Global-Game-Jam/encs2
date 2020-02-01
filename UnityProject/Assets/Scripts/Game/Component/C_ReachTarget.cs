d0581ba7b5c354fa14de4a414af1ece58b22ca17
Hugo Matsumoto <hugo@oktagongames.com>
2020-02-01 12:31:19 -0300

    change: reach target complete

---
+++ UnityProject/Assets/Scripts/Game/Component/C_ReachTarget.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+
+[GenerateAuthoringComponent]
+public struct C_ReachTarget : IComponentData
+{
+    public float3 TargetPosition;
+    public float ReachDistance;
+}
