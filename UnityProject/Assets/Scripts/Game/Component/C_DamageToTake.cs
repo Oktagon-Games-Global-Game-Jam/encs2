2bc6653c14273904a6f05105d771690244eceabe
rafael <raafael.mooraes@gmail.com>
2020-02-01 13:40:28 -0300

    Take damage system added.

---
+++ UnityProject/Assets/Scripts/Game/Component/C_DamageToTake.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct C_DamageToTake : IComponentData
+{
+    public uint DamageToTake;
+
+}
