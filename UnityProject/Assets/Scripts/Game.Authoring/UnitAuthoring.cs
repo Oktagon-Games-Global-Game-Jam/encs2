c547ec78ca155b1a0d07aea5f3d83eb6c97bf01f
Felipexox <felipeaa100@gmail.com>
2020-02-01 23:51:24 -0300

    Added Orc

--- UnityProject/Assets/Scripts/Game.Authoring/UnitAuthoring.cs
+++ UnityProject/Assets/Scripts/Game.Authoring/UnitAuthoring.cs
@@ -12,6 +12,7 @@ public class UnitAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     public Vector3 TargetPosition;
     public float ReachDistance;
     public E_MechaPart MechaPart;
+    public float ActionDistance;
     [Header("Life")]
     public int MaxLife;
     public int ActualLife;
@@ -27,7 +28,7 @@ public class UnitAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     public virtual void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         dstManager.AddComponentData(entity, new C_Move() { Speed = m_MoveSpeed });
-        dstManager.AddComponentData(entity, new C_ReachTarget() { ReachDistance = ReachDistance, TargetPosition = TargetPosition });
+        dstManager.AddComponentData(entity, new C_ReachTarget() { ReachDistance = ReachDistance, TargetPosition = TargetPosition, ActionDistance = ActionDistance});
         dstManager.AddComponentData(entity, new C_MechaPart() { MechaPart = MechaPart});
         dstManager.AddComponentData(entity, new C_Life() { MaxLife = MaxLife, ActualLife = ActualLife });
         dstManager.AddComponentData(entity, new Prefab());
