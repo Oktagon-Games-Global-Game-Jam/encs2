8827556896d7aaf28cdf4685c0d408c2151fa182
Felipexox <felipeaa100@gmail.com>
2020-02-02 13:36:25 -0300

    Sync Animation

--- UnityProject/Assets/Scripts/Game.Authoring/SyncAnimationAuthoring.cs
+++ UnityProject/Assets/Scripts/Game.Authoring/SyncAnimationAuthoring.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 
 public class SyncAnimationAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
+    [SerializeField] private Animator m_Reference;
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        throw new System.NotImplementedException();
+        if( World.AllWorlds[0].GetOrCreateSystem<ECSAnimationSync>().m_Animations == null)
+            World.AllWorlds[0].GetOrCreateSystem<ECSAnimationSync>().m_Animations = new ECSComponentMono<Animator>();
+        World.AllWorlds[0].GetOrCreateSystem<ECSAnimationSync>().m_Animations.AddObject(m_Reference.GetInstanceID(), m_Reference);
+        
+        dstManager.AddComponent(entity, typeof(C_SyncAnimationMono));
+        dstManager.SetComponentData(entity, new C_SyncAnimationMono
+        {
+            Id = m_Reference.GetInstanceID(),
+            State = E_State.Walking
+        });
     }
 }
-
